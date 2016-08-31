d3a0ae7b5351bb5119852bba8e4efedcc73cf070
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2016-08-31 10:31:52 +0200

    Add basic webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    entry: "./app/app.js",
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
